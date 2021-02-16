31d63196cdaec4523bfb15a91317c821178389a5
jgettie <76451312+jgettie@users.noreply.github.com>
2021-02-16 15:18:38 -0500

    Add project files.

---
+++ Create Code for List of Fields/MainForm.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace PersonalProjects.General
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtFieldList = new System.Windows.Forms.TextBox();
+            this.grpCodeForEachField = new System.Windows.Forms.GroupBox();
+            this.txtCodeTemplate = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnCreateCode = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.grpCodeForEachField.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtFieldList);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(581, 193);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Field List";
+            // 
+            // txtFieldList
+            // 
+            this.txtFieldList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtFieldList.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFieldList.Location = new System.Drawing.Point(3, 16);
+            this.txtFieldList.Multiline = true;
+            this.txtFieldList.Name = "txtFieldList";
+            this.txtFieldList.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtFieldList.Size = new System.Drawing.Size(575, 174);
+            this.txtFieldList.TabIndex = 1;
+            this.txtFieldList.WordWrap = false;
+            // 
+            // grpCodeForEachField
+            // 
+            this.grpCodeForEachField.Controls.Add(this.txtCodeTemplate);
+            this.grpCodeForEachField.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpCodeForEachField.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpCodeForEachField.Location = new System.Drawing.Point(3, 202);
+            this.grpCodeForEachField.Name = "grpCodeForEachField";
+            this.grpCodeForEachField.Size = new System.Drawing.Size(581, 193);
+            this.grpCodeForEachField.TabIndex = 3;
+            this.grpCodeForEachField.TabStop = false;
+            this.grpCodeForEachField.Text = "Code For Each Field";
+            // 
+            // txtCodeTemplate
+            // 
+            this.txtCodeTemplate.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtCodeTemplate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodeTemplate.Location = new System.Drawing.Point(3, 16);
+            this.txtCodeTemplate.Multiline = true;
+            this.txtCodeTemplate.Name = "txtCodeTemplate";
+            this.txtCodeTemplate.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtCodeTemplate.Size = new System.Drawing.Size(575, 174);
+            this.txtCodeTemplate.TabIndex = 2;
+            this.txtCodeTemplate.Text = "Private Const FIELDS_%FLD% As String = \"%FLD%\"";
+            this.txtCodeTemplate.WordWrap = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.grpCodeForEachField, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnCreateCode, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(587, 428);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // btnCreateCode
+            // 
+            this.btnCreateCode.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCreateCode.Location = new System.Drawing.Point(3, 401);
+            this.btnCreateCode.Name = "btnCreateCode";
+            this.btnCreateCode.Size = new System.Drawing.Size(581, 23);
+            this.btnCreateCode.TabIndex = 4;
+            this.btnCreateCode.Text = "Create Code";
+            this.btnCreateCode.UseVisualStyleBackColor = true;
+            this.btnCreateCode.Click += new System.EventHandler(this.btnCreateCode_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(591, 629);
+            this.splitContainer1.SplitterDistance = 432;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtResults);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(587, 189);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Results";
+            // 
+            // txtResults
+            // 
+            this.txtResults.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResults.Location = new System.Drawing.Point(3, 16);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResults.Size = new System.Drawing.Size(581, 170);
+            this.txtResults.TabIndex = 2;
+            this.txtResults.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 629);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Create Code for List of Fields";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.grpCodeForEachField.ResumeLayout(false);
+            this.grpCodeForEachField.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtFieldList;
+        private System.Windows.Forms.GroupBox grpCodeForEachField;
+        private System.Windows.Forms.TextBox txtCodeTemplate;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnCreateCode;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtResults;
+
+    }
+}
+
---
+++ Create Code for List of Fields/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PersonalProjects.General
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateCode_Click(object sender, EventArgs e)
+        {
+            txtResults.Text = string.Empty;
+
+            var fields = txtFieldList.Text.Split('\n');
+            foreach (var fld in fields)
+            {
+                if (fld.Trim() != string.Empty)
+                    CreateCodeForField(fld.Trim());
+            }
+        }
+
+        private void CreateCodeForField(string fld)
+        {
+            if (txtResults.Text != string.Empty)
+                txtResults.Text += Environment.NewLine;
+            txtResults.Text += txtCodeTemplate.Text.Replace("%FLD%", fld);
+        }
+    }
+}
